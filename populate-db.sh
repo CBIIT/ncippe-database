cf667ddfeacea6317e3761224689ba4bb3baf860
Debapriya Sarkar <debapriya.sarkar@publicissapient.com>
2019-12-16 20:48:02 -0500

    Added delete script for jenkins

--- populate-db.sh
+++ populate-db.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 echo "Dropping existing database"
-mysqlsh --mysql --sql -u biobank -h $2 -P3306 -p$3 < delete-dbs.sql
+mysql -u biobank -h $2 -P3306 -p$3 < delete-dbs.sql
 
 for f in ./$1/*.sql
 do 
     echo "Processing File $f"
-    mysqlsh --mysql --sql -u biobank -h $2 -P3306 -p$3 < $f
+    mysql -u biobank -h $2 -P3306 -p$3 < $f
 done
