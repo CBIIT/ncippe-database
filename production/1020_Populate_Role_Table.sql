3ba5e5d37d74fa607d882e8983499fdd2ed59530
Debapriya Sarkar <deba.sarkar@gmail.com>
2019-12-19 09:27:11 -0500

    NCIPPE-338 Login as Content Admin (#4)
    Added Content Editor Role and Code

--- production/1020_Populate_Role_Table.sql
+++ production/1020_Populate_Role_Table.sql
@@ -6,4 +6,5 @@ values
     ( 'ROLE_PPE_ADMIN', 'PPE Portal Administrator Role'),
     ( 'ROLE_PPE_CRC', 'Clinical Research Coordinator Role ' ),
     ( 'ROLE_PPE_BSSC', 'BioSpecimen Sample Site Coordinator Role' ),
-    ( 'ROLE_PPE_MOCHA_ADMIN', 'Mocha admin who can upload results');
+    ( 'ROLE_PPE_MOCHA_ADMIN', 'Mocha admin who can upload results'),
+    ( 'ROLE_PPE_CONTENT_EDITOR', 'PPE Content Editor');
