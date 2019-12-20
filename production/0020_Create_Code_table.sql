214414f86ac8c40a5bdbad06e40c96545d597371
Debapriya Sarkar <debapriya.sarkar@publicissapient.com>
2019-12-20 14:23:36 -0500

    NCIPPE-369 NCIPPE| CRC cannot upload consent form Changed the Code Table PK to be non-incrementing Provide explicit Code Id values to avoid changes

--- production/0020_Create_Code_table.sql
+++ production/0020_Create_Code_table.sql
@@ -1,11 +1,10 @@
 USE biobank;
 CREATE TABLE `Code` (
-  `CodeId` int(11) NOT NULL AUTO_INCREMENT,
+  `CodeId` int(11) NOT NULL PRIMARY KEY,
   `CodeName` varchar(32) NOT NULL,
   `Description` varchar(255) DEFAULT NULL,
   `CodeCategoryId` int(11) NOT NULL,
   `IsActive` tinyint(4) NOT NULL DEFAULT '1',
-  PRIMARY KEY (`CodeId`),
   UNIQUE KEY `CodeName_UNIQUE` (`CodeName`),
   KEY `FK_CodeCategory_idx` (`CodeCategoryId`),  
   CONSTRAINT `FK_CodeCategory` FOREIGN KEY (`CodeCategoryId`) REFERENCES `CodeCategory` (`CodeCategoryId`) ON DELETE NO ACTION ON UPDATE NO ACTION
