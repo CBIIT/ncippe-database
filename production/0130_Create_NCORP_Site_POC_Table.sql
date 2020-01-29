3b00b122fc0817d9e1eb073068816405f2529559
Debapriya Sarkar <deba.sarkar@gmail.com>
2020-01-29 09:08:35 -0500

    NCIPPE-412 Add list of Sites to the database Fix typo

--- production/0130_Create_NCORP_Site_POC_Table.sql
+++ production/0130_Create_NCORP_Site_POC_Table.sql
@@ -3,9 +3,9 @@ CREATE TABLE NCORPSitePOC
 (
     NCORPSitePOCId INT(11) NOT NULL PRIMARY KEY, -- primary key column
     POCName VARCHAR(128) NOT NULL,
-    NCORPSiteContactAddressId INT(11) NOT NULL,
+    NCORPSitePOCContactAddressId INT(11) NOT NULL,
 	LastRevisedDate datetime NOT NULL DEFAULT NOW(),
     LastRevisedUser int(11) NULL,
     CONSTRAINT `FK_NCORPSitePOC_LastReviseUser` FOREIGN KEY (`LastRevisedUser`) REFERENCES `User` (`UserId`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-    CONSTRAINT `FK_NCORPSitePOC_POCContactAddressId` foreign key (`POCContactAddressId`) references `ContactAddress` (`ContactAddressId`) ON DELETE NO ACTION ON UPDATE NO ACTION
+    CONSTRAINT `FK_NCORPSitePOC_POCContactAddressId` foreign key (`NCORPSitePOCContactAddressId`) references `ContactAddress` (`ContactAddressId`) ON DELETE NO ACTION ON UPDATE NO ACTION
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
