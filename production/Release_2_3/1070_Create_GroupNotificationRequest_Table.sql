1b72dee6571b56181c51bb1041777fe42362072c
Debapriya Sarkar <deba.sarkar@gmail.com>
2021-08-02 12:55:38 -0400

    NCIPPE-554 Modify Notification request table to store the request details

--- production/Release_2_3/1070_Create_GroupNotificationRequest_Table.sql
+++ production/Release_2_3/1070_Create_GroupNotificationRequest_Table.sql
@@ -1,8 +1,12 @@
 USE biobank;
 CREATE TABLE `GroupNotificationRequest` (
-  `RequestId` INT NOT NULL,
+  `RequestId` INT NOT NULL AUTO_INCREMENT,
   `RequesterId` INT NOT NULL,
   `RequestDateTime` DATETIME NOT NULL DEFAULT NOW(),
+  `MessageEnglish` TEXT(8192) NOT NULL,
+  `MessageSpanish` TEXT(8192) NOT NULL,
+  `SubjectEnglish` VARCHAR(255) NULL,
+  `SubjectSpanish` VARCHAR(255) NULL,
   PRIMARY KEY (`RequestId`),
-  CONSTRAINT `FK_GrpNotReq_RequesterId` FOREIGN KEY (`RequesterId`) REFERENCES `User` (`UserId`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `FK_GrpNotReq_RequesterId` FOREIGN KEY (`RequesterId`) REFERENCES `User` (`UserId`) ON DELETE NO ACTION ON UPDATE NO ACTION
 );
