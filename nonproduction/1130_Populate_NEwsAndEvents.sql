812360b5878ee59106aef8d7ac5a268585bddfbf
sarkard@nih.gov <sarkard@nih.gov>
2022-08-15 15:54:40 -0400

    MB-30  change alert functionality

--- nonproduction/1130_Populate_NEwsAndEvents.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO biobank.`NewsAndEvents` (ContentType, ExpirationDate, Title, Author, Link, LastRevisedUser)
-VALUES 
-( 'news', '2032-01-01', 'This is Some News', 'Dr. Shannon McCall', 'https://cancer.gov', 1),
-( 'event', '2032-01-01', 'This is Some Event', null, 'https://moonshotbiobank.cancer.gov', 1);
