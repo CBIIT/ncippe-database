c7aed7e27fe8e88e3912264f397d31ca360b23fa
sarkard@nih.gov <sarkard@nih.gov>
2022-08-15 18:01:48 -0400

    MB-23 News and Events Section - Release 2.6 Added Events items

---
+++ production/Release_2_6/1140_PopulateNewsAndEvents.sql
@@ -0,0 +1,8 @@
+INSERT INTO biobank.`NewsAndEvents` (ContentType, PublishedDate, ExpirationDate, Title, Author, Link, LastRevisedUser)
+VALUES 
+( 'event', '2022-07-15', '2032-01-01', 'Seminar: Next Generation Biobanking: Operational and Regulatory Requirements for Reporting Results Clinically February 2022', null, 'https://biospecimens.cancer.gov/newsevents/news/20220413_biobanking.asp ', 1),
+( 'event', '2022-07-15', '2032-01-01', 'Mini-Symposium: Maximizing the Research Use of Small Biopsy Tissue February 2022', null, 'https://biospecimens.cancer.gov/newsevents/news/20220413_maximizing.asp', 1),
+( 'event', '2022-07-15', '2032-01-01', 'Mini-Symposium: Cancer Moonshot Biobank Ethical, Legal and Social Implications (ELSI) June 2021', null, 'https://biospecimens.cancer.gov/newsevents/news/20220413_cancer_moonshot.asp', 1),
+( 'event', '2022-07-15', '2032-01-01', 'Development of the Department of Defense Biobank Network and the APOLLO Research Program May 2021', null, 'https://biospecimens.cancer.gov/newsevents/news/20210708_shriver.asp', 1),
+( 'event', '2022-07-15', '2032-01-01', 'Becoming a Culturally Responsive Biobank: A Journey with the Indigenous Mâori People of New Zealand March 2021', null, 'https://biospecimens.cancer.gov/newsevents/news/20210708_morrin.asp', 1),
+( 'event', '2022-07-15', '2032-01-01', '“Permission to Contact”: An Integrated Strategy for Patients and Scientists to Improve Engagement and Enrollment in Biobanking and Clinical Research November 2020', null, 'https://biospecimens.cancer.gov/newsevents/news/20210708_watson.asp', 1);
